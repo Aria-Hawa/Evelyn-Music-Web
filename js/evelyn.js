58c430996f7c21e996d764bd0c9a4195de2ef95d
Aria-Hawa <shigototravailler@gmail.com>
2024-12-26 14:24:43 +0800

    1226-加入音樂和針頭控制

--- js/evelyn.js
+++ js/evelyn.js
@@ -10,102 +10,6 @@ $(function () {
     let rotationId;
     // 用來控制旋轉的 interval
     let rotateInterval;
-    // $('#pin').click(function () {
-    //     if (pinOut) {
-    //         // 清除pin動畫
-    //         $('#pin').css('animation', 'none');
-    //         $('#pin').css('transform', 'rotate(0deg)');
-    //         if (rotateInterval) {
-    //             clearInterval(rotateInterval);
-    //         }
-    //         // 旋轉唱片機到角落
-    //         if (!$('#center').hasClass('moveCorner')) {
-    //             $('#center').addClass('moveCorner');
-    //             if ($(window).width() <= 1280) {
-    //                 $('#center.moveCorner').css({
-    //                     'transform': 'rotate(42deg) translate3d(-41%, 80%, 0)',
-    //                 });
-    //             } if ($(window).width() <= 820) {
-    //                 $('#center.moveCorner').css({
-    //                     'transform': 'rotate(42deg) translate3d(-61%, 40%, 0)',
-    //                 });
-    //             } if ($(window).width() > 1280) {
-    //                 $('#center.moveCorner').css({
-    //                     'transform': 'rotate(42deg) translate3d(-41%, 63%, 0)',
-    //                     'transition': '2s 0.8s all ease-in-out',
-    //                 });
-    //             }
-    //             // 隱藏Evelyn 及 playlist字眼
-    //             $('#center.moveCorner').find('h1,p').delay(1200).fadeOut(900);
-    //             // 顯示logo
-    //             $('header').delay(2000).fadeIn(800);
-    //         }
-    //         // 延遲2秒開始旋轉唱片
-    //         setTimeout(function () {
-    //             rotateInterval = setInterval(function () {
-    //                 deg = (deg + 1) % 360; // 每次增加1度
-    //                 $('.rotateImg').css('transform', `rotate(${deg}deg) scale(0.95)`);
-    //             }, 15); // 每15毫秒更新一次角度
-    //         }, 2000);
-    //         // 延遲3秒開始播放音樂，並讓 pin 開始轉動
-    //         setTimeout(function () {
-    //             $('#pin').css('animation', `rotatePin linear ${playAudio.duration}s forwards`);
-    //             $('#pin').css('animation-play-state', 'running'); // 恢復 pin 轉動
-    //             playAudio.play();
-    //             if ($(playAudio).find('source').attr('src') == './tryAudio/snoozyBeats-lazyAfternoon.mp3') {
-    //                 // 顯示HOME內容
-    //                 $('#evelynHome h2').delay(800).fadeIn(1800, function () {
-    //                     $('#evelynHome p').fadeIn(2000, function () {
-    //                         $('#audioLicense').fadeIn(800);
-    //                     });
-    //                 });
-
-    //             } else {
-    //                 $('#audioLicense').remove();
-    //                 $('#audioLicense2').delay(800).fadeIn(2500);
-    //             }
-    //         }, 3000);
-    //         pinOut = false;
-    //     } else {
-    //         // 延遲1.4s停止旋轉+暫停pin
-    //         setTimeout(function () {
-    //             if (rotateInterval) {
-    //                 clearInterval(rotateInterval);
-    //                 rotateInterval = null;
-    //             }
-    //             // 暫停音樂
-    //             playAudio.pause();
-    //             // 暫停 pin 的動畫
-    //             $('#pin').css('animation-play-state', 'paused');
-    //         }, 1400);
-    //         pinOut = true; // 更新狀態
-    //     }
-    // });
-
-
-
-
-    // // 音樂結束ended，停止旋轉+pin歸位
-    // playAudio.onended = function () {
-    //     // 停止旋轉
-    //     clearInterval(rotateInterval);
-    //     // pin歸位
-    //     $('#pin').css({
-    //         'animation': 'pinReturn 1.8s ease-in-out forwards',
-    //     });
-    //     // 淡出音樂license
-    //     $('#audioLicense').fadeOut(2500, function () {
-    //         $('#evelynHome p').fadeOut(2500, function () {
-    //             $('#evelynHome h2').fadeOut(2500)
-    //         });
-    //     });
-    //     $('#audioLicense2').fadeOut(2500);
-    //     pinOut = true;
-    // }
-
-
-
-
 
 
 
@@ -166,7 +70,7 @@ $(function () {
             }, 2000);
         } else {
             // pin在內時click為暫停or繼續播放
-            // 停止播放
+            // 暫停播放
             if (!$('#pin').hasClass('pause')) {
                 $('#pin').addClass('pause');
                 setTimeout(function () {
@@ -198,13 +102,35 @@ $(function () {
     });
 
     // 音樂結束ended，停止旋轉+pin歸位
-    playAudio.onended = function () {
-        // 停止旋轉
-        clearInterval(rotateInterval);
-        // pin先remove.beClick(抬高)
-        $('#pin').removeClass('beClick');
+    // 獨立函示
+    function pinReturnAnimation() {
+        // 紀錄pin當前角度
+        let currentDeg = $('#pin').css('transform');
+        let values = currentDeg.split('(')[1].split(')')[0].split(',');
+        let a = values[0];
+        let b = values[1];
+        let angle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
+
+        // 動態生成pinReturn動畫
+        let styleSheet = document.styleSheets[0];
+        let keyframes = `
+            @keyframes pinReturn {
+                from {
+                    transform: rotate(${angle}deg);
+                }
+                to {
+                    transform: rotate(7deg);
+                }
+            }
+        `;
+        if (styleSheet.cssRules) {
+            styleSheet.insertRule(keyframes, styleSheet.cssRules.length);
+        } else if (styleSheet.rules) {
+            styleSheet.addRule(keyframes, styleSheet.rules.length);
+        }
+
         setTimeout(function () {
-            // pin 1.2s後歸位(回到7deg)
+            // pin 1.8s後歸位(回到7deg)
             $('#pin').css({
                 'animation': 'pinReturn 1.8s ease-in-out forwards',
             });
@@ -214,9 +140,17 @@ $(function () {
                     $(this).css('animation', '');
                 }
             });
-
         }, 1800);
-        // 必須刪除剛剛的animation-pinReturn,否則會影響到我pin下次新增.beClick時候的動畫效果
+    }
+    // 獨立函示
+    function audioEnd() {
+        // 停止旋轉
+        clearInterval(rotateInterval);
+        // pin先remove.beClick(抬高)
+        $('#pin').removeClass('beClick');
+        pinReturnAnimation();
+        // 移除#pin的pause類別
+        $('#pin').removeClass('pause');
 
         // 淡出音樂license
         $('#audioLicense').fadeOut(2500, function () {
@@ -225,12 +159,20 @@ $(function () {
             });
         });
         $('#audioLicense2').fadeOut(2500);
-    }
+    };
+    // 音樂結束後呼叫audioEnd
+    playAudio.onended = audioEnd;
     // 以上11/29測試
 
 
 
     // 以下拖拉唱片(12/1 測試使用GSAP的拖放功能)
+    const audioSrc = {
+        'homeRecord': './tryAudio/snoozyBeats-lazyAfternoon.mp3',
+        'aboutRecord': './tryAudio/snoozyBeats-midnightDrifter.mp3',
+        'worksRecord': './tryAudio/snoozyBeats-doingGood.mp3',
+        'contactRecord': './tryAudio/snoozyBeats-rewind.mp3',
+    };
 
     Draggable.create(".draggable", {
         type: "x,y",
@@ -252,15 +194,34 @@ $(function () {
                 draggableRect.right > dropzoneRect.left;
 
             if (insideZone) {
+                // 暫停pin動畫
+                $('#pin').css('animation', 'none');
+
+                // droppable裡面的唱片切換顯示
                 const isBlock = $('#droppable').find('.insideDroppable').filter(function () {
                     return $(this).css('display') == 'block';
                 });
                 $(isBlock).css('display', 'none');
                 $(`.insideDroppable.${draggable.id}`).css('display', 'block');
+                // 目前display:block的唱片要設定是rotateImg
+                $('.rotateImg').removeClass('rotateImg').css('transform', '');
+                const newRotateImg = $(`.insideDroppable.${draggable.id}`).children().last();
+                newRotateImg.addClass('rotateImg').css('transform', 'rotate(0deg) scale(0.95)');
 
+                // 重置旋轉角度
+                deg = 0;
+                if (rotateInterval) {
+                    clearInterval(rotateInterval);
+                }
+                rotateInterval = setInterval(function () {
+                    deg = (deg + 1) % 360;
+                    $('.rotateImg').css('transform', `rotate(${deg}deg) scale(0.95)`);
+                }, 15);
+
+                // album的rotate切換
                 $('.album').css('rotate', '');
                 $(`.album.${draggable.id}`).css('rotate', '-5deg');
-
+                // 讓被拖拉的唱片回到原位並切換顯示
                 gsap.to(draggable, {
                     zIndex: 0,
                     x: 0,
@@ -275,8 +236,18 @@ $(function () {
                 });
                 $('.albumRecord.draggable').css('display', 'block');
                 $(draggable).css('display', 'none');
+                // 切換音源
+                audioEnd();
+                const audioKey = $(draggable).attr('data-src');
+                const audioNow = audioSrc[audioKey];
+                $('#playAudio source').attr('src', audioNow);
+                playAudio.load();
+
+                // 恢復pin動畫
+                setTimeout(function () {
+                    $('#pin').css('animation', '');
+                }, 2000);
             } else {
-                console.log(this.target);
                 gsap.to(draggable, {
                     zIndex: 0,
                     x: 0,
@@ -292,8 +263,6 @@ $(function () {
                     }
                 });
             };
-
-
         },
     });
     // 寫在draggable後面，避免我hover時候rotate失效
@@ -310,120 +279,6 @@ $(function () {
     $('#about .card h4').click(function () {
         $(this).next().toggleClass('beclick');
     });
-
-
-
-    // 以下9/19測試更改
-    // // 初始化draggable
-    // $(".draggable").draggable({
-    //     revert: "invalid",
-    //     start: function (event, ui) {
-    //         $('#menuList').css('z-index', '2');
-    //         // 停止音樂
-    //         playAudio.pause();
-    //         // 停止旋轉
-    //         clearInterval(rotateInterval);
-    //         // pin歸位
-    //         $('#pin').css({
-    //             'animation': 'pinReturn 1.8s ease-in-out forwards',
-    //         });
-    //         // 淡出音樂license
-    //         $('#audioLicense').fadeOut(2500);
-    //         $('#audioLicense2').fadeOut(2500);
-    //         pinOut = true;
-    //     },
-    //     drag: function (event, ui) {
-    //         $('#contactHome').css('animation', 'fadeOutTopLeft 1.8s forwards');
-    //         $('.rotateImg').removeClass('rotateImg');
-    //         $("#droppable").find(".draggable").fadeOut(800);
-    //     },
-    // });
-
-    // // function () {
-    // //     let thisDiv = $(this).clone().attr('style', '').css('display', 'none');
-    // //     let thisId = thisDiv.attr('id');
-    // //     $(`.${thisId}`).find('.albumBasic').after(function () {
-    // //         return $(thisDiv).fadeIn(800);
-    // //     });
-    // //     $(this).remove();
-    // //     thisDiv.draggable();
-    // // }
-
-    // // 初始化droppable
-    // $("#droppable").droppable({
-    //     accept: ".draggable",
-    //     drop: function (event, ui) {
-    //         // 9/19變更
-    //         // #droppable裡面的.draggable要移除
-    //         let currentItem = $(this).find(".draggable");
-    //         console.log(currentItem)
-
-
-
-
-
-    //         var $droppedItem = ui.helper;
-    //         // 將新的物件放入droppable
-    //         $(this).append($droppedItem);
-    //         $droppedItem.css({
-    //             position: "relative",
-    //             left: 0,
-    //             top: 0
-    //         });
-    //         $droppedItem.find('img:last').addClass('rotateImg');
-    //         if (!$('#center').hasClass('moveCorner')) {
-    //             $('#center').addClass('moveCorner');
-    //             if ($(window).width() <= 1280) {
-    //                 $('#center.moveCorner').css({
-    //                     'transform': 'rotate(42deg) translate3d(-41%, 80%, 0)',
-    //                 });
-    //             } else {
-    //                 $('#center.moveCorner').css({
-    //                     'transform': 'rotate(42deg) translate3d(-41%, 63%, 0)',
-    //                     'transition': '2s 0.8s all ease-in-out',
-    //                 });
-    //             }
-    //         }
-
-    //         $('#center.moveCorner').find('h1,p').delay(1200).fadeOut(900);
-    //         $('header').delay(2000).fadeIn(800);
-    //         $('#content').delay(2400).fadeIn(800);
-    //         switch ($droppedItem.attr('id')) {
-    //             case 'aboutRecord':
-    //                 $('.inner').css('display', 'none');
-    //                 $('#about.inner').fadeIn(900);
-    //                 $('#playAudio source').attr('src', './tryAudio/snoozyBeats-midnightDrifter.mp3');
-    //                 $('#audioLicense2 span').text('＜Midnight Drifter＞');
-    //                 playAudio = $('#playAudio')[0];
-    //                 playAudio.load();
-    //                 break;
-    //             case 'worksRecord':
-    //                 $('.inner').css('display', 'none');
-    //                 $('#works.inner').fadeIn(900);
-    //                 $('#playAudio source').attr('src', './tryAudio/snoozyBeats-doingGood.mp3');
-    //                 $('#audioLicense2 span').text('＜Doing Good＞');
-    //                 playAudio = $('#playAudio')[0];
-    //                 playAudio.load();
-    //                 break;
-    //             case 'contactRecord':
-    //                 $('.inner').css('display', 'none');
-    //                 $('#contact.inner').fadeIn(900);
-    //                 $('#playAudio source').attr('src', './tryAudio/snoozyBeats-rewind.mp3');
-    //                 $('#audioLicense2 span').text('＜Rewind＞');
-    //                 playAudio = $('#playAudio')[0];
-    //                 playAudio.load();
-    //                 break;
-    //         }
-
-    //     }
-    // });
-    // 以上9/19測試更改
-
-
-
-
-
-
 
 
 
